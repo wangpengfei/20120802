851dcbef70712366d56a1d640802e861a78b49c0
wangpengfei <wangpengfeiking@gmail.com>
2012-08-21 08:54:29 +0800

    写的excel转换成html的工具类

--- TestJava/src/com/gtoyg/exceltohtml/TestMain.java
+++ TestJava/src/com/gtoyg/exceltohtml/TestMain.java
@@ -1,6 +1,11 @@
 package com.gtoyg.exceltohtml;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.apache.poi.hssf.converter.ExcelToHtmlConverter;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.w3c.dom.Document;
 
 public class TestMain {
@@ -9,8 +14,27 @@ public class TestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Document doc = null;
-		ExcelToHtmlConverter etohtml = new ExcelToHtmlConverter(doc);
+
+		try {
+			FileInputStream fin = new FileInputStream("g:\\2011.xls");
+
+			ToHtml toHtml = ToHtml.create(fin, System.out);
+
+			toHtml.setCompleteHTML(true);
+			toHtml.printPage();
+
+			// HSSFWorkbook book = new HSSFWorkbook(fin);
+			//
+			// Document doc = null;
+			// ExcelToHtmlConverter etohtml = new ExcelToHtmlConverter(doc);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
